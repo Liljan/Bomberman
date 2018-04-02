0cda7eab9d1ab26921c44f511ecbb514310e49c2
LordMegaDeath3KHaxxor <marli166@student.liu.se>
2018-04-02 10:05:49 +0200

    Object Pooling.

--- Bomberman/Assets/Scripts/BombSpawner.cs
+++ Bomberman/Assets/Scripts/BombSpawner.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class BombSpawner : MonoBehaviour {
+public class BombSpawner : MonoBehaviour
+{
+    public Camera camera;
+    public Tilemap tileMap;
+    public ObjectPool bombPool;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Awake()
+    {
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 worldPos = camera.ScreenToWorldPoint(Input.mousePosition);
+            worldPos.z = 0.0f;
+
+            bombPool.SpawnObject(worldPos, Quaternion.identity);
+
+        }
+    }
 }
---
+++ Bomberman/Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectPool : MonoBehaviour
+{
+    public GameObject originalInstance;
+    public int size;
+    public bool isExpandable;
+
+    private List<GameObject> _pool;
+
+
+    private void Awake()
+    {
+        _pool = new List<GameObject>(size);
+
+        for (int i = 0; i < size; i++)
+        {
+            GameObject newGameObject = (GameObject)Instantiate(originalInstance);
+            newGameObject.SetActive(false);
+            // _pool[i] = newGameObject;
+            _pool.Add(newGameObject);
+        }
+    }
+
+    public bool SpawnObject(Vector3 position, Quaternion rotation)
+    {
+        for (int i = 0; i < _pool.Count; i++)
+        {
+            GameObject obj = _pool[i];
+            if (!obj.activeInHierarchy)
+            {
+                obj.transform.position = position;
+                obj.transform.rotation = rotation;
+                obj.SetActive(true);
+
+                return true;
+            }
+        }
+
+        if (isExpandable)
+        {
+            GameObject obj = Instantiate(originalInstance);
+            obj.transform.position = position;
+            obj.transform.rotation = rotation;
+            obj.SetActive(true);
+
+            _pool.Add(obj);
+
+            return true;
+        }
+
+        return false;
+    }
+}
